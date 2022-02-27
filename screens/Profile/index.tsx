ad3812dab6b2df339e377701f68e27395ff4f46f
Mushfiq Rashid <mushfiqarashid@gmail.com>
2022-02-27 08:50:39 -0600

    Add profile page

--- screens/Profile/index.tsx
+++ screens/Profile/index.tsx
@@ -1,36 +1,81 @@
-import { StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Image,AppRegistry,
+} from 'react-native';
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import { RootTabScreenProps } from "../../types";
 
-export default function Profile(){
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>profile</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      >
+export default class Profile extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+          <View style={styles.header}></View>
+            <Image
+          style={styles.tinyLogo}
+          source={{uri: 'https://reactnative.dev/img/tiny_logo.png'}}
+        />
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>John Doe</Text>
+              <Text style={styles.info}>Trust Score: 90%</Text>
+              <Text style={styles.description}>Richardson, TX</Text>
+            </View>
+          <View style={styles.body}>
+        </View>
       </View>
-    </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+    container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
+  header:{
+    backgroundColor: "#219653",
+    height:200,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#4CAD73",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
+    flex: 1,
+    alignItems: 'center',
+    padding:30,
+  },
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
+  },
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
